439de447a2c2db86981439b09aa398d87d645ca9
Nicole Lee <lnicole8282@gmail.com>
2026-09-14 18:08:11 +0800

    Updates to cleaning up heatmap and climbing renaming

--- NLCLIMB.py
+++ NLCLIMB.py
@@ -297,7 +297,7 @@ def generation(df, driver):
     
     fps = frames(df)
     
-    #falling occurence
+    #climbing occurence
     dff_dark = df[(df['ExperimentState'] == 'Assimilation time - Dark') | (df['ExperimentState']== 'Dark')].iloc[:-4]
     dff_light = df[(df['ExperimentState'] == 'Assimilation time - Full') | (df['ExperimentState']== 'Full')].iloc[:-4]
     dff_rec = df[(df['ExperimentState']== 'Recovery')] 
